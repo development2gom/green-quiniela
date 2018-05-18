c8d79a1d138f80a9aeb46a12c5d8d41ae7bc6d2b
Raul Alanis <rulosc07@gmail.com>
2018-05-18 08:26:14 -0500

    action de partidos por fase

--- controllers/ConcursantesController.php
+++ controllers/ConcursantesController.php
@@ -1,14 +1,46 @@
 <?php
 namespace app\controllers;
 
+use Yii;
 use yii\web\Controller;
 use app\models\WrkPartidos;
 use yii\db\Expression;
 use app\models\CatEquipos;
+use app\models\RelUsuariosCodigos;
+use app\models\CatFasesDelTorneo;
 
 
 class ConcursantesController extends Controller{
  
+    /**
+     * @inheritdoc
+     */
+    /*public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControlExtend::className(),
+                'only' => ['partidos-fase'],
+                'rules' => [
+                    [
+                        'actions' => ['partidos-fase'],
+                       'allow' => true,
+                        'roles' => ['usuario-normal'],
+
+                        
+                    ],
+                    
+                ],
+            ],
+           // 'verbs' => [
+           //     'class' => VerbFilter::className(),
+           //     'actions' => [
+           //         'logout' => ['post'],
+           //     ],
+           // ],
+       ];
+   }*/
+
     public function actionInstrucciones()
     {
         return $this->render('instrucciones');
@@ -16,8 +48,7 @@ class ConcursantesController extends Controller{
 
     public function actionPartidosProximos()
     {
-        $partidos =WrkPartidos::find()->where(['b_habilitado'=>1])->
-        andWhere(['>','fch_partido',new Expression('now()')])->all() ;
+        $partidos = WrkPartidos::find()->where(['b_habilitado'=>1])->andWhere(['>','fch_partido',new Expression('now()')])->all();
         return $this->render('partidos-proximos',['partidos'=>$partidos]);
 
     }
@@ -33,7 +64,11 @@ class ConcursantesController extends Controller{
         return $this->render('index');
     }
 
+    public function actionPartidosFase(){
+        $user = Yii::$app->user->Identity;
+        $codigo = RelUsuariosCodigos::find()->where(['id_usuario'=>$user->id_usuario])->one();
+        $fase = CatFasesDelTorneo::find()->where(['id_fase'=>$codigo->codigo->id_fase])->one();
+        $partidos = WrkPartidos::find()->where(['id_fase'=>$fase->id_fase])->all();print_r($partidos);exit;
+    }
 }
-
-
 ?>
