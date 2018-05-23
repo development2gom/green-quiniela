19cd5eb3082b54a868872b4c7963b101db01a2a6
Raul Alanis <rulosc07@gmail.com>
2018-05-23 12:58:36 -0500

    cambios al seleccionar ganador por partido

--- controllers/ConcursantesController.php
+++ controllers/ConcursantesController.php
@@ -68,16 +68,19 @@ class ConcursantesController extends Controller
         return $this->render('partidos-proximos',['partidos'=>$partidos]);
 
     }
+
     public function actionResultados()
     {
         return $this->render('resultados');
     }
+
     public function actionLideres()
     {
         //consulta a la base de datos
         $equipos = CatEquipos::find()->where(['b_habilitado' => 1])->orderBy('num_puntuacion')->all();
         return $this->render('lideres', ['equipos' => $equipos]);
     }
+
     public function actionAdministrador()
     {
         return $this->render('index');
@@ -85,77 +88,83 @@ class ConcursantesController extends Controller
 
     public function actionGuardarResultados()
     {
-//crear un if para conpara la face  del catalogo de torneo y la fase de los partidos y son iguales poder segir con el gusrdado
-
-
-
-        $idUsuario = 3;
-
         $response = new ResponseServices();
+        //crear un if para conpara la face  del catalogo de torneo y la fase de los partidos y son iguales poder segir con el gusrdado
+        
+        /**
+        * TODO: Cambiar id_usuario a id de usuario logueado
+        */
+        $idUsuario = 3;
 
         $token = null;
         $partido_seleccionado = null;
 
         if (isset($_POST['token'])) {
-
             $token = $_POST['token'];
 
         } else {
             $response->message = 'falta parametro del partido';
             return $response;
         }
-//se reciben las variables por post y se verifica por mmedio del isset si esta no se encuentra vacia
-//posteriormente se aloja dentro de una variable
+        //se reciben las variables por post y se verifica por mmedio del isset si esta no se encuentra vacia
+        //posteriormente se aloja dentro de una variable
         if (isset($_POST['equipo_seleccionado'])) {
             $partido_seleccionado = $_POST['equipo_seleccionado'];
-
         }
-//camel keys
-        $existeQuiniela = WrkQuiniela::find()->where(['id_usuario' => $idUsuario])->andWhere(['=', 'id_partido', new Expression('(select id_partido from wrk_partidos
-            where b_habilitado = 1
-            and txt_token ="' . $token . '")')])->one();
-
-        if ($existeQuiniela) {
-
-            $response->message = "El resgistro ya se encuentra guardado";
-
-            return $response;
-
-        }
+        //camel keys
         
-    //consulta a la base de datos
+        //consulta a la base de datos
         $faseTorneo = CatFasesDelTorneo::find()->where(['b_habilitado' => 1])->andWhere(['between', new Expression('now()'), new Expression('fch_inicio'), new Expression('fch_termino')])
             ->one();
 
-        $resultado = WrkPartidos::find()->where(['b_habilitado' => 1])->andWhere(['' => $token])->one();
-
+        $resultado = WrkPartidos::find()->where(['b_habilitado' => 1])->andWhere(['txt_token' => $token])->one();
 
         if ($faseTorneo->id_fase != $resultado->id_fase) {
             $response->message = "El tiempo de la fase ha expirado";
 
             return $response;
-
         }
-    //se le asigna a la variable quiniela todo el contenido que existe en la tabla wrkquiniela
-        $quiniela = new WrkQuiniela();
-            //por medio de las flechitas se busca llegar a el parametro necesitado para posteriormene alojarlo en labase de datos
-        $quiniela->id_partido = $resultado->id_partido;
-        $quiniela->id_usuario = $idUsuario;
-        $quiniela->fch_creacion = Calendario::getFechaActual();
 
+        $existeQuiniela = WrkQuiniela::find()->where(['id_usuario' => $idUsuario])->andWhere(['=', 'id_partido', new Expression('(select id_partido from wrk_partidos
+        where b_habilitado = 1
+        and txt_token ="' . $token . '")')])->one();
 
-        if ($partido_seleccionado) {
-            $quiniela->id_equipo_ganador = $partido_seleccionado;
+        if ($existeQuiniela) {
+            
+            
+            if ($partido_seleccionado) {
+                $existeQuiniela->id_equipo_ganador = $partido_seleccionado;
+                $existeQuiniela->b_empata = 0;
+            } else {
+                $existeQuiniela->b_empata = 1;
+                $existeQuiniela->id_equipo_ganador = null;
+            }
 
-        } else {
-            $quiniela->b_empata = 1;
-        }
-//envia el contenido de quiniela a la base de datos
-        
+            if ($existeQuiniela->save()) {
+                $response->status = 'success';
+                $response->message = 'resgistro guardado';
+            }
+        }else{
 
-        if ($quiniela->save()) {
-            $response->status = 'success';
-            $response->message = 'resgistro guardado';
+            //se le asigna a la variable quiniela todo el contenido que existe en la tabla wrkquiniela
+            $quiniela = new WrkQuiniela();
+                //por medio de las flechitas se busca llegar a el parametro necesitado para posteriormene alojarlo en labase de datos
+            $quiniela->id_partido = $resultado->id_partido;
+            $quiniela->id_usuario = $idUsuario;
+            $quiniela->fch_creacion = Calendario::getFechaActual();
+
+            
+            if ($partido_seleccionado) {
+                $quiniela->id_equipo_ganador = $partido_seleccionado;
+            } else {
+                $quiniela->b_empata = 1;
+            }
+            //envia el contenido de quiniela a la base de datos
+
+            if ($quiniela->save()) {
+                $response->status = 'success';
+                $response->message = 'resgistro guardado';
+            }
         }
         return $response;
     }
--- web/webAssets/js/site/proximos-partidos.js
+++ web/webAssets/js/site/proximos-partidos.js
@@ -1,61 +1,49 @@
 //recibe la variable
-$(document).ready(function(){
-    $(".js-equipo").on("click",function(){
-//console.log($(this).data('token'));
-var token=$(this).data('partido');
-var equipo_seleccionado =$(this).data('token');
-var resultado =$(this).data('nombre');
+$(document).ready(function () {
+    $(".js-equipo").on("click", function () {
+        //console.log($(this).data('token'));
+        var token = $(this).data('partido');
+        var equipo_seleccionado = $(this).data('token');
+        var resultado = $(this).data('nombre');
+        var url = $(".text-completa-registra").data('url');
 
-//se verifica si la variable esta bacia
-if(!equipo_seleccionado){
-    equipo_seleccionado = null;
-}
+        //se verifica si la variable esta bacia
+        /*if (!equipo_seleccionado) {
+            equipo_seleccionado = null;
+        }*/
 
-swal({
-    title: "Espera",
-    text: "¿Esta seguro de guardar el resultado seleccionado:"+resultado+"?",
-    type: "warning",
-    showCancelButton: true,
-    confirmButtonClass: "btn-warning",
-    confirmButtonText: "Sí, estoy seguro",
-    cancelButtonText: "No, revisaré una vez más",
-    closeOnConfirm: true,
-    //closeOnCancel: false
-},
-function() {
-    
-    $.ajax({
-        url:'http://localhost:81/clientes/green/green-quiniela/web/concursantes/guardar-resultados',
-        type: 'post',
-        data:{
-            token:token,
-            equipo_seleccionado: equipo_seleccionado
-            
-    
-        },
-        success:function(resultado){
-            if(resultado.status == 'success'){
-                swal('Correcto','Resultados guardados con exito','success');
+        $.ajax({
+            url: url+'/concursantes/guardar-resultados',
+            type: 'post',
+            data: {
+                token: token,
+                equipo_seleccionado: equipo_seleccionado
+            },
+            success: function (resultado) {
+                if (resultado.status == 'success') {
+                    //swal('Correcto', 'Resultados guardados con exito', 'success');
+                    $("#js-div-partido-"+token).removeClass('js-partido-no-contestado');
+                }
+                else {
+                    swal('Espera', resultado.message, 'error');
+                }
+
+            },
+            error: function () {
+                swal('Espera', 'Ocurrio un problema al guardar el resultado', 'error');
             }
-            else{
-                swal('Espera',resultado.message,'error');
-            }
-   
-        },
-        error: function()
-        {
-            swal('Espera','Ocurrio un problema al guardar el resultado','error');
+
+        });
+    });
+
+    $("#js-verificar-siguiente").on('click', function(){
+        var url = $(this).data('url');
+        var sinContestar = $(".js-partido-no-contestado");
+        if(sinContestar.length > 0){
+            swal('Espera', 'Falta por contestar '+sinContestar.length+' partidos', 'warning');            
+        }else{
+            window.location.href = url+"/concursantes/termino";
         }
-    
     });
-});
-
-
-
-//envia la bariable a otro lado
-
-
-    });
-
 });
 
