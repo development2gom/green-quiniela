b80d78228ab11b78c3d72fd42f28b04b4a64e9de
Damián <damian2gom@gmail.com>
2018-05-23 12:21:40 -0500

    Pantallas maquetadas

--- views/layouts/classic/topBar/mainTerminosCondiciones.php
+++ views/layouts/classic/topBar/mainTerminosCondiciones.php
@@ -3,9 +3,9 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 use yii\helpers\Html;
-use app\assets\AppAssetBienvenido;
+use app\assets\AppAssetTerminosCondiciones;
 use yii\helpers\Url;
-AppAssetBienvenido::register($this);
+AppAssetTerminosCondiciones::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -18,37 +18,19 @@ AppAssetBienvenido::register($this);
         <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
 
-  <div class="triangulo-naranja">
-    <img src="<?=Url::base()?>/webAssets/images/TRIANGULO-NARANJA.png" alt="">
-  </div>
-
-  <div class="pasto-superior-derecha">
-    <img src="<?=Url::base()?>/webAssets/images/PASTO-VERDE-SUP_DER.png" alt="">
-  </div>
-
-  <div class="red-balon">
-    <img src="<?=Url::base()?>/webAssets/images/RED_BALON.png" alt="">
-  </div>
-
   <div class="pasto-verde-inferior-izquierda">
     <img src="<?=Url::base()?>/webAssets/images/PASTO-VERDE_INF_IZQ.png" alt="">
   </div>
 
-  
-
-  <div class="logo-25-anos">
-    <img src="<?=Url::base()?>/webAssets/images/LOGO-25-ANOS-CSF-01.png" alt="">
-  </div>
-
-  <a href="<?=Url::base()?>/" class="logo-quiniela">
-    <img src="<?=Url::base()?>/webAssets/images/LOGO-QUINIELA-MUNDIALISTA-01.png" alt="">
+  <a href="<?=Url::base()?>/site/terminos" class="logo-quiniela-terminos">
+    <img src="<?=Url::base()?>/webAssets/images/LOGO-QUINIELA-MUNDIALISTA_AZUL.png" alt="">
   </a>
 
   <?php $this->beginBody();?>
   
 
-  <div class="page-bienvenida vertical-align text-center" data-animsition-in="fade-in" data-animsition-out="fade-out">
-    <div class="page-content vertical-align-middle animation-slide-top animation-duration-1">
+  <div class="page-terminos-condiciones" data-animsition-in="fade-in" data-animsition-out="fade-out">
+    <div class="page-content animation-slide-top animation-duration-1">
       <?=$content?>
     </div>
   </div>  
