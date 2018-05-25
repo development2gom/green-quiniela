23e3b7c0e4d2fbf94a80db5c864691d9346042e8
Damián <damian2gom@gmail.com>
2018-05-25 09:36:26 -0500

    Actualizar partidos - Usuarios / Maquetación

--- views/administrador/actualizar-partidos.php
+++ views/administrador/actualizar-partidos.php
@@ -1,28 +1,35 @@
 <?php
-
-
+$this->title = "Actualizar Partidos";
+$this->params['classBody'] = "sec-actualizar-partidos";
 $this->registerJsFile('@web/webAssets/js/site/actualizar-partidos.js',
-['depends'=>[\app\assets\AppAsset::className()]]);
+['depends'=>[\app\assets\AppAssetClassicTopBar::className()]]);
 
 ?>
-<?php
-foreach ($partidos as $partidos) {
+<div class="sec-ap-cont">
+
+    
+    <?php
+    foreach ($partidos as $partidos) {
     $equipo1 = $partidos->equipo1;
     $equipo2 = $partidos->equipo2;
-    echo $equipo1->txt_nombre_equipo;
     ?>
-    <img src='<?=$equipo1->txt_url_imagen_equipo;?>' data-nombre="<?=$equipo1->txt_nombre_equipo;?>"  data-token ="<?=$partidos->txt_token?>" data-equipo="<?=$partidos->id_equipo1?>"
-    class="js-equipos"/>
-    <button class='js-equipos' data-nombre='Empate' data-token ="<?=$partidos->txt_token?>">empate</button >
+        <div class="sec-ap-item">
+            <div class="sec-ap-item-local">
+                <img src='<?=$equipo1->txt_url_imagen_equipo;?>' data-nombre="<?=$equipo1->txt_nombre_equipo;?>"  data-token ="<?=$partidos->txt_token?>" data-equipo="<?=$partidos->id_equipo1?>" class="js-equipos"/>
+                <p><?=  $equipo1->txt_nombre_equipo;?></p>
+            </div>
+            
+            <div class="sec-ap-item-empate">
+                <button class='btn btn-primary js-equipos' data-nombre='Empate' data-token ="<?=$partidos->txt_token?>">empate</button >
+            </div>
+            <div class="sec-ap-item-visita">
+                <img src= '<?=$equipo2->txt_url_imagen_equipo;?>' data-nombre='<?=$equipo2->txt_nombre_equipo?>' data-token ="<?=$partidos->txt_token?>" data-equipo="<?=$partidos->id_equipo2?>" class="js-equipos">
+                <p><?= $equipo2->txt_nombre_equipo;?></p>
+            </div>
+        </div>
     <?php
-
-    echo $equipo2->txt_nombre_equipo;
+    }
     ?>
-    <img src= '<?=$equipo2->txt_url_imagen_equipo;?>' data-nombre='<?=$equipo2->txt_nombre_equipo?>' data-token ="<?=$partidos->txt_token?>" data-equipo="<?=$partidos->id_equipo2?>"
-    class="js-equipos">
-    <?php
     
-    echo '<br><br>';
-}
 
-?>
+</div>
