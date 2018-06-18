5714042f602407fb663b99c3772790c8c818366f
matiasdgom <alfonso@2gom.com.mx>
2018-06-18 14:30:21 -0500

    actualizacion de partidos

--- views/administrador/actualizar-partidos.php
+++ views/administrador/actualizar-partidos.php
@@ -13,7 +13,6 @@ $this->registerJsFile(
 ?>
 
 
-
 <div class="sec-np-tabs-cont">
     <div class="nav-tabs-horizontal" data-plugin="tabs">
         <ul class="nav nav-tabs" role="tablist">
@@ -22,7 +21,10 @@ $this->registerJsFile(
 
             ?>
             <li class="nav-item" role="presentation">
-                <a class="nav-link" data-toggle="tab" href="#p<?= $faseTorneo->id_fase ?>" aria-controls="p<?= $faseTorneo->id_fase ?>" role="tab" aria-expanded="false">
+                <a class="nav-link" data-toggle="tab" 
+                href="#p<?= $faseTorneo->id_fase ?>" 
+                aria-controls="p<?= $faseTorneo->id_fase ?>" 
+                role="tab" aria-expanded="false">
                     <?= $faseTorneo->txt_nombre_fase; ?>
                 </a>
             </li>
@@ -37,7 +39,10 @@ $this->registerJsFile(
                     foreach ($fases as $faseTorneo) {
 
                         ?>
-                        <a class="nav-link" data-toggle="tab" href="#p<?= $faseTorneo->id_fase ?>" aria-controls="p<?= $faseTorneo->id_fase ?>" role="tab" aria-expanded="false">
+                        <a class="nav-link" data-toggle="tab" 
+                        href="#p<?= $faseTorneo->id_fase ?>" 
+                        aria-controls="p<?= $faseTorneo->id_fase ?>" 
+                        role="tab" aria-expanded="false">
                             <?= $faseTorneo->txt_nombre_fase; ?>
                         </a>
                     <?php 
@@ -58,10 +63,14 @@ $this->registerJsFile(
     foreach ($fases as $faseTorneo) {
 
 
-        $partidos = WrkPartidos::find()->where(['b_habilitado' => 1])->andWhere(['is not', 'id_equipo1', null])->andWhere(['is not', 'id_equipo2', null])->andWhere(['id_fase' => $faseTorneo->id_fase])
-            ->all();
+        $partidos = WrkPartidos::find()->
+        where(['b_habilitado' => 1])->
+        andWhere(['is not', 'id_equipo1', null])->
+        andWhere(['is not', 'id_equipo2', null])->
+        andWhere(['id_fase' => $faseTorneo->id_fase])->
+        all();
         ?>
-<div class="tab-pane " id="p<?= $faseTorneo->id_fase ?>" role="tabpanel" aria-expanded="false">
+<div class="tab-pane active" id="<?= $faseTorneo->id_fase ?>" role="tabpanel" aria-expanded="false">
 
 <?php
 
@@ -72,23 +81,33 @@ $this->registerJsFile(
 
     ?>
 
-<div id='js-seleccion-<?= $nuevosPartidos->txt_token ?>' class="sec-ap-item js-partido-<?= $nuevosPartidos->id_partido ?>">
-            <div class="sec-ap-item-local js-seleccionar-equipo js-equipos <?= $nuevosPartidos->id_equipo1 == $nuevosPartidos->id_equipo_ganador ? 'active' : '' ?>" data-nombre="<?= $equipo1->txt_nombre_equipo; ?>" data-token ="<?= $nuevosPartidos->txt_token ?>" data-equipo="<?= $nuevosPartidos->id_equipo1 ?>">
+<div id='js-seleccion-<?=$nuevosPartidos->txt_token ?>' class="sec-ap-item js-partido-<?= $nuevosPartidos->id_partido ?>">
 
+            <div class="sec-ap-item-local js-seleccionar-equipo js-equipos 
+            <?= $nuevosPartidos->id_equipo1 == $nuevosPartidos->id_equipo_ganador ? 'active' :''?>" 
+            data-nombre="<?= $equipo1->txt_nombre_equipo; ?>" data-token ="<?= $nuevosPartidos->txt_token ?>" 
+            data-equipo="<?= $nuevosPartidos->id_equipo1 ?>">
                 <img src='<?= $equipo1->txt_url_imagen_equipo; ?>'/>
                 <p><?= $equipo1->txt_nombre_equipo; ?></p>
             </div>
-           
+            
         
         
-        <div class="sec-ap-item-empate <?= $nuevosPartidos->b_empate == 1 ? 'active' : '' ?>">
-                <button class='btn btn-secondary js-seleccionar-equipo js-equipos' 
+        <div class="sec-ap-item-empate 
+        ">
+                <button class='btn btn-secondary js-seleccionar-equipo js-equipos <?= $nuevosPartidos->b_empate == 1 ? 'active' : '' ?>'  
                 data-nombre='Empate' data-token ="<?= $nuevosPartidos->txt_token ?>" 
                 data-equipo="<?= null ?>">empate</button >
             </div>
-            
-            <div class="sec-ap-item-visita js-seleccionar-equipo js-equipos <?= $nuevosPartidos->id_equipo2 == $nuevosPartidos->id_equipo_ganador ? 'active' : '' ?>" data-nombre='<?= $equipo2->txt_nombre_equipo ?>' data-token ="<?= $nuevosPartidos->txt_token ?>" data-equipo="<?= $nuevosPartidos->id_equipo2 ?>">
+
+
+            <div class="sec-ap-item-visita js-seleccionar-equipo js-equipos
+             <?= $nuevosPartidos->id_equipo2 == $nuevosPartidos->id_equipo_ganador ? 'active' : '' ?>" 
+             data-nombre='<?= $equipo2->txt_nombre_equipo ?>' data-token ="<?= $nuevosPartidos->txt_token ?>" 
+             data-equipo="<?= $nuevosPartidos->id_equipo2 ?>">
+
                 <img src= '<?= $equipo2->txt_url_imagen_equipo; ?>' >
+
                 <p><?= $equipo2->txt_nombre_equipo; ?></p>
             </div>
 
